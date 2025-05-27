c09358adba57bea87629f8820e01c082060029e3
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-05-27 23:29:41 +0530

    feat(impl): implement token bucket using Mutex

--- internal/limiter/token_bucket.go
+++ internal/limiter/token_bucket.go
@@ -12,3 +12,38 @@ type TokenBucket struct {
 	lastRefilledTime time.Time
 	mu               sync.Mutex
 }
+
+func NewTokenBucket(capacity, refillRate int) *TokenBucket {
+	return &TokenBucket{
+		capacity:         capacity,
+		tokens:           capacity,
+		refillRate:       refillRate,
+		lastRefilledTime: time.Now(),
+	}
+}
+
+func (tb *TokenBucket) Allow() bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+
+	now := time.Now()
+
+	// calculate how many tokens to refill
+	elaspedTime := now.Sub(tb.lastRefilledTime).Seconds()
+	tb.tokens += int(elaspedTime * float64(tb.refillRate))
+
+	// don't exceed bucket capacity
+	if tb.tokens > tb.capacity {
+		tb.tokens = tb.capacity
+	}
+
+	// update last refill time
+	tb.lastRefilledTime = now
+
+	// allow request only if tokens are available
+	if tb.tokens > 0 {
+		tb.tokens--
+		return true
+	}
+	return false
+}
