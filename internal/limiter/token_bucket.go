9168a8e7ee5fbc8622c52ba0b45f1a562e01d99d
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-05-27 23:05:47 +0530

    feat(models): define token_bucket struct

--- internal/limiter/token_bucket.go
+++ internal/limiter/token_bucket.go
@@ -1,1 +1,14 @@
 package limiter
+
+import (
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	capacity         int
+	tokens           int
+	refillRate       int
+	lastRefilledTime time.Time
+	mu               sync.Mutex
+}
