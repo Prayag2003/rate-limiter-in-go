de3d3f44d299eb291adeca62baa00b000c45a9b5
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-05-27 23:48:00 +0530

    feat(main): simulate the limiting buckets

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/Prayag2003/rate-limiter-in-go/internal/limiter"
+	"github.com/Prayag2003/rate-limiter-in-go/internal/simulator"
+)
+
+func main() {
+	tokenBucketLimiter := limiter.NewTokenBucket(100, 5)
+	leakyBucketLimiter := limiter.NewLeakyBucket(100, 50)
+
+	simulator.SimulateLimiter("TokenBucket", tokenBucketLimiter, 1000, 10)
+	simulator.SimulateLimiter("LeakyBucket", leakyBucketLimiter, 1000, 10)
+}
